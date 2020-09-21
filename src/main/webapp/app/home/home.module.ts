a1564b9a8e3dfc4605acfe996d39cef95fac688c
mandre <maxime.andre@amadeus.com>
2020-09-21 11:40:20 +0200

    design for trip recap with harcoded values

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 import { JhipstertestSharedModule } from 'app/shared/shared.module';
+import { TripRecapComponent } from 'app/trip-recap/trip-recap.component';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-  imports: [JhipstertestSharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent],
+  imports: [JhipstertestSharedModule, RouterModule.forChild([HOME_ROUTE]), MatCardModule, MatDividerModule, MatButtonModule, MatIconModule],
+  declarations: [HomeComponent, TripRecapComponent],
+  exports: [],
 })
 export class JhipstertestHomeModule {}
---
+++ src/main/webapp/app/trip-recap/trip-recap.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-trip-recap',
+  templateUrl: './trip-recap.component.html',
+  styleUrls: ['./trip-recap.component.scss'],
+})
+export class TripRecapComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
