fd4410fe8e215d2f5617ae84977b539374729542
mandre <maxime.andre@amadeus.com>
2020-09-24 00:00:19 +0200

    Handle correctly db data in trip recap with moment tz

--- src/main/webapp/app/trip-recap/trip-recap.component.ts
+++ src/main/webapp/app/trip-recap/trip-recap.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Trip } from 'app/shared/models/trip.model';
+import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'jhi-trip-recap',
@@ -6,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trip-recap.component.scss'],
 })
 export class TripRecapComponent implements OnInit {
+  @Input() trip!: Trip;
+
+  departureDateAndTime!: moment.Moment;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // IMPROVEMENT display in UTC? Why?
+    this.departureDateAndTime = moment.tz(this.trip.departureDate + ' ' + this.trip.departureTime, this.trip.departureUtcZone);
+  }
 }
